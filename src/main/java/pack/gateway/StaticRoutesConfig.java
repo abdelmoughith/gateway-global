370c4ccf2df98d318c704c8c8f19e989cf5ce0f2
MOUGHIT <abdelmoughith2002@gmail.com>
2025-11-30 15:07:44 +0100

    gateway

---
+++ src/main/java/pack/gateway/StaticRoutesConfig.java
@@ -0,0 +1,18 @@
+package pack.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StaticRoutesConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/clients/**").uri("lb://SERVICE-CLIENT"))
+                .build();
+    }
+}
+
