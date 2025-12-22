0ec788784f114baed3e0157377e7056286a3e7e6
MOUGHIT <abdelmoughith2002@gmail.com>
2025-12-22 11:18:05 +0100

    swagger works

--- src/main/java/pack/gateway/security/SecurityConfig.java
+++ src/main/java/pack/gateway/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package pack.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -9,12 +10,17 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .cors(ServerHttpSecurity.CorsSpec::disable)
-                .authorizeExchange(ex -> ex.anyExchange().permitAll())
+                .authorizeExchange(ex -> ex
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyExchange().permitAll()
+                )
                 .build();
     }
 }
---
+++ src/main/java/pack/gateway/utils/UrlUtils.java
@@ -0,0 +1,6 @@
+package pack.gateway.utils;
+
+public class UrlUtils {
+    public static final String REACT_URL_LOCALHOST = "http://localhost:5173";
+    public static final String REACT_URL = "http://10.80.65.69:5173";
+}
